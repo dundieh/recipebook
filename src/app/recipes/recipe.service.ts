d9f0c79496a5d5604f369e6d655b557b151d672f
ToGermanScore Ltd <yousef@togermanscore.com>
2021-10-02 14:24:32 +0200

    dev commit

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService {
+    recipesChanged = new Subject<Recipe[]>();
+
     private recipes: Recipe[] = [
         new Recipe(
             'Red Sauce Pasta',
-            'Italian Meal',
+            'Italian Meal - Bellisimo',
             'https://www.wikihow.com/images/a/aa/Prepare-Spicy-Macaroni-with-Tomato-Sauce-Step-5.jpg',
             [
                 new Ingredient('red sauce', 1),
@@ -17,7 +20,7 @@ export class RecipeService {
         ),
         new Recipe(
             'Beef Burger',
-            'American Fast Food',
+            'American Fast Food - just awesome!',
             'https://www.thespruceeats.com/thmb/POpuxXZ8hoq56_m7KUPcy41clvo=/2668x2001/smart/filters:no_upscale()/indian-style-burger-1957599-hero-01-266103a4bb4e4ee7b5feb4da2d2e99da.jpg',
             [
                 new Ingredient('Beef Meat', 2),
@@ -35,6 +38,21 @@ export class RecipeService {
 
     getRecipe(index: number) {
         return this.recipes[index];
+    }
+
+    addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    deleteRecipe(index: number) {
+        this.recipes.splice(index, 1);
+        this.recipesChanged.next(this.recipes.slice());
     }
 
     addIngredientToShoppingList(ingredients: Ingredient[]) {
