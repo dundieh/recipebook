9f5a03eb417d772cd8c5d6d4adc28bb00c480317
ToGermanScore Ltd <yousef@togermanscore.com>
2021-10-09 14:33:21 +0200

    dev commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromApp from './store-rx/app.reducer';
 import * as AuthActions from './auth/store-rx/auth.actions';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,10 @@ import * as AuthActions from './auth/store-rx/auth.actions';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(private store: Store<fromApp.AppState>) {}
+  constructor(private store: Store<fromApp.AppState>, private router: Router) {}
 
   ngOnInit() {
     this.store.dispatch(new AuthActions.AutoLogin());
+    this.router.navigate(['/recipes']);
   }
 }
